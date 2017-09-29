833f9f1f37990d0a30e84ad7a80db27fd87e841a
okaychen <1440281580@qq.com>
2017-09-29 22:50:40 +0800

    updata user page <dorpDown>

--- pages/user/user.js
+++ pages/user/user.js
@@ -4,14 +4,47 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    selectPerson: true,
+    firstPerson: '个人',
+    selectArea: false,
   },
+
+  
   //事件处理函数
   bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
+
+
+  //点击选择类型
+  clickDown: function () {
+    var selectPerson = this.data.selectPerson;
+    if (selectPerson == true) {
+      this.setData({
+        selectArea: true,
+        selectPerson: false,
+      })
+    } else {
+      this.setData({
+        selectArea: false,
+        selectPerson: true,
+      })
+    }
+  },
+  //点击切换
+  mySelect: function (e) {
+    this.setData({
+      firstPerson: e.target.dataset.me,
+      selectPerson: true,
+      selectArea: false,
+    })
+  },
+
+
+
   onLoad: function () {
     console.log('onLoad')
     var that = this
@@ -23,8 +56,6 @@ Page({
       })
     })
 
-    var menuOne = document.querySelectorAll('menu-one');
-    console.log(menuOne);
 
   }
 })
