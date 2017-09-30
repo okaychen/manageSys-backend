09b20a2a408feca5a0d15285ffa868a9fd4d9557
okaychen <1440281580@qq.com>
2017-09-30 22:22:38 +0800

    complete detail's page

--- pages/pintuan/details/details.js
+++ pages/pintuan/details/details.js
@@ -10,4 +10,89 @@ Page({
     interval: 3000,
     duration: 1000
   },
+
+  showModal1: function () {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData1: animation.export(),
+      showModalStatus1: true
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData1: animation.export()
+      })
+    }.bind(this), 200)
+  },
+  hideModal1: function () {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData1: animation.export(),
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData1: animation.export(),
+        showModalStatus1: false
+      })
+    }.bind(this), 200)
+  },
+
+  showModal2: function () {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData2: animation.export(),
+      showModalStatus2: true
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData2: animation.export()
+      })
+    }.bind(this), 200)
+  },
+  hideModal2: function () {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData2: animation.export(),
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData2: animation.export(),
+        showModalStatus2: false
+      })
+    }.bind(this), 200)
+  }
+
+
+
 })
