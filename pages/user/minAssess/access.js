170732cc0ee3cb71554a0bbd6690b88773a95f97
okaychen <1440281580@qq.com>
2017-09-30 12:30:59 +0800

    complete access pages

--- pages/user/minAssess/access.js
+++ pages/user/minAssess/access.js
@@ -6,6 +6,7 @@ Page({
     selectedSrc: '../../../image/selected.png',
     halfSrc: '../../../image/half.png',
     key: 1,//评分
+    path: ''
   },
   onLoad: function () {
   },
@@ -30,5 +31,50 @@ Page({
     this.setData({
       key: key
     })
+  },
+
+
+
+   upload: function () {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths)
+        wx.uploadFile({
+          url: 'http://example.weixin.qq.com/upload',
+          filePath: tempFilePaths[0],
+          name: 'file',
+          formData: {
+            'user': 'test'
+          },
+          success: function (res) {
+            var data = res.data
+            wx.showModal({
+              title: '上传文件返回状态',
+              content: '成功',
+              success: function (res) {
+                if (res.confirm) {
+                  console.log('用户点击确定')
+                }
+              }
+            })                          //do something
+          },
+          fail: function (res) {
+            console.log(res)
+          }
+        })
+        that.setData({
+          path: tempFilePaths
+        })
+      }
+    })
   }
+
+
+
+
 })
