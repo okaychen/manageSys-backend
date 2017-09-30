847a134db8e7467aeb04a3e705a9b4357ca5ddf6
okaychen <1440281580@qq.com>
2017-09-30 11:30:20 +0800

    split image from public <structure>

--- pages/user/minAssess/access.js
+++ pages/user/minAssess/access.js
@@ -1,66 +1,33 @@
-// pages/user/minAssess/access.js
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    stars: [0, 1, 2, 3, 4],
+    normalSrc: '../../images/normal.png',
+    selectedSrc: '../../images/selected.png',
+    halfSrc: '../../images/half.png',
+    key: 0,//评分
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad: function () {
   },
+  //点击右边,半颗星
+  selectLeft: function (e) {
+    var key = e.currentTarget.dataset.key
+    if (this.data.key == 0.5 && e.currentTarget.dataset.key == 0.5) {
+      //只有一颗星的时候,再次点击,变为0颗
+      key = 0;
+    }
+    console.log("得" + key + "分")
+    this.setData({
+      key: key
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  //点击左边,整颗星
+  selectRight: function (e) {
+    var key = e.currentTarget.dataset.key
+    console.log("得" + key + "分")
+    this.setData({
+      key: key
+    })
   }
 })
