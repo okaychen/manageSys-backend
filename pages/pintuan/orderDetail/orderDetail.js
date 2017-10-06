61d5aacda88608a27e8e958649b72d8d60e29a73
Jeffrey <1737723363@qq.com>
2017-10-06 20:12:40 +0800

    对订单详情页面的倒计时进行判断(如果不是拼团购买,则不显示倒计时)

--- pages/pintuan/orderDetail/orderDetail.js
+++ pages/pintuan/orderDetail/orderDetail.js
@@ -31,50 +31,52 @@ Page({
 	onReady: function () {
 		let that = this;
 		let order = this.data.order;
-		let current_time = parseInt(Date.parse(new Date()) / 1000);
-		let totalSecond = 86400 - ( current_time - parseInt(order.get_group.create_time));
-		console.log(current_time);
-		console.log(parseInt(order.get_group.create_time));
-		if (totalSecond < 0) {//结束
-			this.setData({
-				time_is_ok: true
-			});
-		} else {
-			var interval = setInterval(function () {
-				// 秒数
-				var second = totalSecond;
-				
-				// 天数位
-				var day = Math.floor(second / 3600 / 24);
-				var dayStr = day.toString();
-				if (dayStr.length == 1) dayStr = '0' + dayStr;
-				
-				// 小时位
-				var hr = Math.floor((second - day * 3600 * 24) / 3600);
-				var hrStr = hr.toString();
-				if (hrStr.length == 1) hrStr = '0' + hrStr;
-				
-				// 分钟位
-				var min = Math.floor((second - day * 3600 * 24 - hr * 3600) / 60);
-				var minStr = min.toString();
-				if (minStr.length == 1) minStr = '0' + minStr;
-				
-				// 秒位
-				var sec = second - day * 3600 * 24 - hr * 3600 - min * 60;
-				var secStr = sec.toString();
-				if (secStr.length == 1) secStr = '0' + secStr;
-				
+		if (order.purchase_method == 'group') {
+			let current_time = parseInt(Date.parse(new Date()) / 1000);
+			let totalSecond = 86400 - ( current_time - parseInt(order.get_group.create_time));
+			console.log(current_time);
+			console.log(parseInt(order.get_group.create_time));
+			if (totalSecond < 0) {//结束
 				this.setData({
-					countDownDay: dayStr,
-					countDownHour: hrStr,
-					countDownMinute: minStr,
-					countDownSecond: secStr
+					time_is_ok: true
 				});
-				totalSecond--;
-			}.bind(this), 1000);
+			} else {
+				var interval = setInterval(function () {
+					// 秒数
+					var second = totalSecond;
+					
+					// 天数位
+					var day = Math.floor(second / 3600 / 24);
+					var dayStr = day.toString();
+					if (dayStr.length == 1) dayStr = '0' + dayStr;
+					
+					// 小时位
+					var hr = Math.floor((second - day * 3600 * 24) / 3600);
+					var hrStr = hr.toString();
+					if (hrStr.length == 1) hrStr = '0' + hrStr;
+					
+					// 分钟位
+					var min = Math.floor((second - day * 3600 * 24 - hr * 3600) / 60);
+					var minStr = min.toString();
+					if (minStr.length == 1) minStr = '0' + minStr;
+					
+					// 秒位
+					var sec = second - day * 3600 * 24 - hr * 3600 - min * 60;
+					var secStr = sec.toString();
+					if (secStr.length == 1) secStr = '0' + secStr;
+					
+					this.setData({
+						countDownDay: dayStr,
+						countDownHour: hrStr,
+						countDownMinute: minStr,
+						countDownSecond: secStr
+					});
+					totalSecond--;
+				}.bind(this), 1000);
+			}
 		}
 		
 	}
-  
+	
 });
 
