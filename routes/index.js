ddb5b8bef9128f34d8d5d0e673bef42a75b278bd
okaychen <1440281580@qq.com>
2019-12-12 22:44:59 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: '后台系统' });
 });
 
 module.exports = router;
